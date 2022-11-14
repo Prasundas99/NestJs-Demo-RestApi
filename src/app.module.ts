64339e26324f9e5c7620e21431d7d994b9fc7c6a
Prasundas99 <prasundas115@gmail.com>
2022-11-14 15:03:21 +0530

    db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import { ItemModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose'
+import keys from './config/keys';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ItemModule, ConfigModule.forRoot(), MongooseModule.forRoot(keys.mongoURI)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
-export class AppModule {}
+export class AppModule { }
