660ffeb7cec3ffa7956e96b316943fc42d7183df
Prasundas99 <prasundas115@gmail.com>
2022-11-15 14:45:16 +0530

    hard coded data

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -11,17 +11,25 @@ import {
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { Request, Response } from 'express';
+import { Item } from './interfaces/item.interface';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemService: ItemsService) {}
   // Express way
-  @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Get all Items..');
-  }
+  //   @Get()
+  //   findAll(@Req() req: Request, @Res() res: Response): Response {
+  //     console.log(req.url);
+  //     return res.send('Get all Items..');
+  //   }
 
   // Nest way: more preffered
+  @Get()
+  findAll(): Item[] {
+    return this.itemService.findAll();
+  }
+
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name}, Desc: ${createItemDto.description}, quantity: ${createItemDto.qty}`;
@@ -30,12 +38,12 @@ export class ItemsController {
   // get params fromm request
   // instead on req.params.id we do it like params.id
   @Get(':id')
-  findOne(@Param('id') id) {
-    return `Item ${id}`
+  findOne(@Param('id') id): Item {
+    return this.itemService.findOne(id);
   }
 
   @Delete(':id')
   delete(@Param('id') id): string {
-    return `Delete ${id}`
+    return `Delete ${id}`;
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [
+        {
+            id: "15ffd",
+            name: "",
+            qty: 5,
+            description: "fnfdjffg"
+        },
+        {
+            id: "15ffd",
+            name: "",
+            qty: 5,
+            description: "fnfdjffg"
+        },
+        {
+            id: "15ffd",
+            name: "",
+            qty: 5,
+            description: "fnfdjffg"
+        },
+    ]
+
+    findAll(): Item[] {
+        return this.items;
+    }
+
+    findOne(id: string): Item {
+        return this.items.find(item => item.id === id);
+    }
+}
