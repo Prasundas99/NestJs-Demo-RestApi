0f37bf623a15541e3eeba749609533398c0a4947
Prasundas99 <prasundas115@gmail.com>
2022-12-02 18:26:01 +0530

    global swagger doc added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo NestJS API')
+    .setDescription('The Demo NestJS API description')
+    .setVersion('1.0')
+    .addTag('docs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(6900);
 }
 bootstrap();
