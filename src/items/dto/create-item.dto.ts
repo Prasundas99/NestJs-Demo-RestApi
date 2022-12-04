0c2e54e685e932b2adfcc6173fdb483412f47024
Prasundas99 <prasundas115@gmail.com>
2022-12-04 20:22:51 +0530

    labled swagger doc

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateItemDto {
+    @ApiProperty()
     readonly name: string;
+    @ApiProperty()
     readonly description: string;
+    @ApiProperty()
     readonly qty: number;
 }
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -7,13 +7,13 @@ import {
     Body,
     Req,
     Res,
-    Param,
+    Param, HttpStatus
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { Request, Response } from 'express';
 import { Item } from './interfaces/item.interface';
 import { ItemsService } from './items.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('items')
 @Controller('items')
@@ -27,6 +27,7 @@ export class ItemsController {
     //   }
 
     // Nest way: more preffered
+    @ApiResponse({ status: HttpStatus.OK, type: [CreateItemDto], description: 'Fetch all items' })
     @Get()
     async findAll(): Promise<Item[]> {
         return await this.itemService.findAll();
@@ -34,21 +35,30 @@ export class ItemsController {
 
     // get params fromm request
     // instead on req.params.id we do it like params.id
+    @ApiCreatedResponse({ type: CreateItemDto })
+    // add params to swager request
+    @ApiParam({ name: 'id', required: true })
     @Get(':id')
     async findOne(@Param('id') id): Promise<Item> {
         return await this.itemService.findOne(id);
     }
 
+    @ApiCreatedResponse({ type: CreateItemDto })
     @Post()
     create(@Body() createItemDto: CreateItemDto): Promise<Item> {
         return this.itemService.create(createItemDto);
     }
 
+    @ApiCreatedResponse({ type: CreateItemDto })
+    // add params to swager request
+    @ApiParam({ name: 'id', required: true })
     @Delete(':id')
     delete(@Param('id') id): Promise<Item> {
         return this.itemService.delete(id);
     }
 
+    @ApiCreatedResponse({ type: CreateItemDto })
+    @ApiParam({ name: 'id', required: true })
     @Put(':id')
     update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
         return this.itemService.update(id, updateItemDto);
